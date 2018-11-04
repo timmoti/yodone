1136e6fb3bae1016f6aac9cc3052acbe19b5caa7
TimothyTan <timmoti@gmail.com>
2018-11-04 17:26:42 +0800

    Add production config to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,7 +3,16 @@ import 'firebase/firestore';
 import 'firebase/auth';
 import swal from 'sweetalert';
 
-const config = {
+const prodConfig = {
+  apiKey: 'AIzaSyAzK34m6sQHAsj2iSYAi2w8nr-94ZnbkX0',
+  authDomain: 'yodone-prod.firebaseapp.com',
+  databaseURL: 'https://yodone-prod.firebaseio.com',
+  projectId: 'yodone-prod',
+  storageBucket: 'yodone-prod.appspot.com',
+  messagingSenderId: '292652706716'
+};
+
+const devConfig = {
   apiKey: 'AIzaSyDPPfMltduYw4u2wlDSK1Qh48AuprtX1bc',
   authDomain: 'yodone-882b6.firebaseapp.com',
   databaseURL: 'https://yodone-882b6.firebaseio.com',
@@ -11,6 +20,8 @@ const config = {
   storageBucket: 'yodone-882b6.appspot.com',
   messagingSenderId: '145491458490'
 };
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);
